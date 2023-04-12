752beac6b3bcc087717871ecc19d2e67ead0d60e
Kevin Tran <kinzerfest@gmail.com>
2023-04-12 16:03:52 -0700

    Updated project version and remove gameprefences

--- Assets/Tools/_Scripts/DataPersistence/GamePreferences.cs
+++ Assets/Tools/_Scripts/DataPersistence/GamePreferences.cs
@@ -7,36 +7,6 @@ namespace _Scripts.DataPersistence
     [Serializable]
     public class GamePreferences
     {
-        [Serializable]
-        public class Game1
-        {
-            public float targetShapeProbability = 0.8f;
-            public float targetEyeProbability = 0.8f;
-            public float targetColorProbability = 0.8f;
-            public float targetSizeProbability = 0.8f;
-            
-            public float targetModelEarlySpawnTime = 10f;
-            public float targetModelLateSpawnTime = 10f;
-            
-            public float earlySpawnTime = 0.5f;
-            public float lateSpawnTime = 3f;
-
-            public int spawnPoints = 10;
-        }
-        
-        public Game1 game1 = new();
-
-        [Serializable]
-        public class Game2
-        {
-            public int aliensToSpawn = 10;
-            public int timeBetweenSpawns = 1;
-            public int spawnGridVertical = 10;
-            public int spawnGridHorizontal = 10;
-        }
-
-        public Game2 game2 = new();
-
         public QuestionOptions[] questionaire = {
             new()
             {
